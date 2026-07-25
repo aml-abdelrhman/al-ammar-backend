5f9cf736eaed490788280976cb9fdc8fca687c3b
Aml Abdelrhman <Aml586651@gmail.com>
2026-07-25 21:06:54 +0300

    Add correct ScholarSeeder file

--- database/seeders/ScholarSeeder.php
+++ database/seeders/ScholarSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Scholar;
 
-class ScholarsSeeder extends Seeder
+class ScholarSeeder extends Seeder
 {
     public function run(): void
     {
